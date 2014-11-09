2ad2745f263746fc28ce81499b68c756c5efa83b
CollinBlinder <collinblinder@gmail.com>
2014-11-09 14:13:21 -0500

    Copied schema and user.rb files from master branch after running a git pull from the upstream in that branch. Pasted those files over their sibling files in the create_controllers4 branch to ammend merge conflicts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-<<<<<<< HEAD
-
-
   has_many :image_sets
-
-
-
-  has_many :image_sets
-
-=======
-  has_many :image_sets
->>>>>>> b7cc79841e83ecf4b4dd98d05c6d9a4e28e2e36a
 end
