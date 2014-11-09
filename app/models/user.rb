9b1c09c1f54ac7d3b3df64fdd2dc2f059ab54212
CollinBlinder <collinblinder@gmail.com>
2014-11-09 14:02:18 -0500

    Rolled back migrations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
   has_many :image_sets
-=======
->>>>>>> 44bb608... update gemlock
-=======
+
+
+
   has_many :image_sets
->>>>>>> b898d49... created image sets migration
+
 end
