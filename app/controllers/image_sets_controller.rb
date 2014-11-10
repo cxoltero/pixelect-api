ebf21e8d02b6d0f1ced89b6033bbbf1b473e9866
cxoltero <carlosh95@gmail.com>
2014-11-09 22:55:25 -0500

    added the update to the image_sets_controller, and changed the create to be able to upload more than one image for the set

--- app/controllers/image_sets_controller.rb
+++ app/controllers/image_sets_controller.rb
@@ -19,9 +19,49 @@ class ImageSetsController < ApplicationController
   # GET /image_sets
   # GET /image_sets.json
   def create
-    @image_set = ImageSet.create(image_set_params)
 
-    render json: @image_set
+    # original code
+
+    # @image_set = ImageSet.create(image_set_params)
+
+    # render json: @image_set
+
+# code for imageMagick
+    @image_set = current_user.image_sets.guild(image_set_params)
+    authorize @image_set
+    if @image_set.save
+      # to handle multiple image upload on create
+      if params[:images]
+        params[:images].each {|image|
+          @image_set.images.create(image: image)
+        }
+      end
+      flash[:notice] = "Your image set has been created."
+      redirect_to @image_set
+    else
+      flash[:alert] = "Something went wrong."
+      render 'new'
+    end
+
+  end
+
+# created for multiple image uploading
+  def update
+    authorize @image_set
+    if @image_set.update(params[:image_set].permit(:title, :description))
+      # to handle multiple images upload on update when user add more pictures
+      if params[:images]
+        params[:images].each {|image|
+          @image_set.images.create(image: image)
+        }
+      end
+
+      flash[:notice] = "Your image set has been updated!."
+      redirect_to @image_set
+    else
+      flash[:alert] = "Something went wrong."
+      render 'edit'
+    end
   end
 
   private
